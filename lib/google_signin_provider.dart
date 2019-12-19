1577b6e0c7076654af469c44a34d226b23b45b4e
Milind Mevada <milind.mevada@solutelabs.com>
2019-12-19 15:46:48 +0530

    Renamed pub

--- lib/google_signin_provider.dart
+++ lib/google_signin_provider.dart
@@ -1,9 +1,9 @@
-library google_signin_provider;
+library flutter_google_signin;
 
 import 'package:auth_provider/auth_provider.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_google_signin/google_signin_provider_impl.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:google_signin_provider/google_signin_provider_impl.dart';
 
 AuthProvider authProvider;
 
